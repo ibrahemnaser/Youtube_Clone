cf26dd4dbec4c2d302f75b0582041278b2bdefcd
ibrahemnaser <i.naser4040@gmail.com>
2022-09-02 15:21:38 +0200

    fix export default

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // one file to export all components
-import { default as Navbar } from "./Navbar";
-import { default as Feed } from "./Fedd";
-import { default as ChannelDetails } from "./ChannelDetails";
-import { default as SearchFeed } from "./SearchFeed";
-import { default as VideoDetails } from "./VideoDetails";
+export { default as Navbar } from "./Navbar";
+export { default as Feed } from "./Feed";
+export { default as ChannelDetails } from "./ChannelDetails";
+export { default as SearchFeed } from "./SearchFeed";
+export { default as VideoDetails } from "./VideoDetails";
